4521325ec507d28d8a40a3d813db83545c5082d7
Yesudeep Mangalapilly <gora.khargosh@gmail.com>
2011-03-09 13:34:05 +0530

    Remove crockford's jsmin.
    Signed-off-by: Yesudeep Mangalapilly <gora.khargosh@gmail.com>

--- geoservice/serializers.py
+++ geoservice/serializers.py
@@ -4,7 +4,6 @@ from tornado.escape import \
     json_encode as _json_encode, \
     json_decode as _json_decode
 from geoservice.jsmin_v8 import JavaScriptMinifier
-from geoservice.jsmin import jsmin as minify
 
 def minify_v8(value):
     minifier = JavaScriptMinifier()
