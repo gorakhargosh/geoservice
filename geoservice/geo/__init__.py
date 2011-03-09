9fd3932a44ccce7751b7915ea7e97cb3eb1665ba
Yesudeep Mangalapilly <gora.khargosh@gmail.com>
2011-03-09 10:13:58 +0530

    Clearer names
    Signed-off-by: Yesudeep Mangalapilly <gora.khargosh@gmail.com>

--- geoservice/geo/__init__.py
+++ geoservice/geo/__init__.py
@@ -7,6 +7,6 @@ import pygeoip
 geoip_dat = resource_filename(__name__, "GeoIP.dat")
 geolite_city_dat = resource_filename(__name__, "GeoLiteCity.dat")
 
-geo_country = pygeoip.GeoIP(geoip_dat)
-geo_city = pygeoip.GeoIP(geolite_city_dat)
+geoip_country = pygeoip.GeoIP(geoip_dat)
+geoip_city = pygeoip.GeoIP(geolite_city_dat)
 
